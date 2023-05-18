a5c085d82954d904269f92c1c9eb5e425d2164d4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-18 09:40:30 -0400

    set up test server with express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('server');
+import express from 'express';
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
+
+const port = 5001;
+app.listen(port, () => console.log(`server is listening on port ${port}`));
