3faf0a3e00b2eb2cc09eb35e1dde74ddcaef834a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-18 10:58:31 -0400

    installed a host of node pacakages

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
     res.send('Welcome');
 });
 
-const port = 5001;
-app.listen(port, () => console.log(`server is listening on port ${port}`));
+const port = process.env.PORT || 5001;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
