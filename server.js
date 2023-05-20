c7b0ae266f2d6be4c2943799eb98f58ae390414b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-20 11:03:33 -0400

    uncommented code for the routes that have files, changed posts to observances

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ import { fileURLToPath } from 'url';
 // import authRoutes from './routes/auth.js';
 // import userRoutes from './routes/users.js';
 // import postRoutes from './routes/posts.js';
-// import {register} from './controllers/auth.js'
-// import {createPost} from './controllers/posts.js'
+import {register} from './controllers/auth.js'
+import {createObservance} from './controllers/posts.js'
 // import { verifyToken } from './middleware/auth.js';
 // import User from './models/User.js';
 // import Post from './models/Post.js';
@@ -43,8 +43,8 @@ const storage = multer.diskStorage({
 const upload =  multer({storage});
 
 //Routes With Files
-// app.post('/auth/register', upload.single('picture'), register);
-// app.post('/posts', verifyToken, upload.single('picture'), createPost);
+app.post('/auth/register', upload.single('picture'), register);
+app.post('/observances', verifyToken, upload.single('picture'), createObservance);
 
 // //Routes not dependent on files/multer
 // app.use('/auth', authRoutes);
