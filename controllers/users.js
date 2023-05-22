5ccc01015bce2fdc58c811d17c76e56862c20b00
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-22 09:57:16 -0400

    created get user and update user controllers

---
+++ controllers/users.js
@@ -0,0 +1,36 @@
+import User from "../models/User";
+import jwt from 'jsonwebtoken';
+
+export const getUser = async (req, res) => {
+  try {
+      const {id} = req.params;
+      const user = await User.findById(id);
+      res.status(200).json(user);
+  } catch (error) {
+      res.status(404).json({message: error.message})
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {stateId, firstName, lastName, congressDist, pollingPlace, picturePath} = req.body;
+    const user = await User.findById(id)
+
+    user.stateId = stateId;
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.congressDist = congressDist;
+    user.pollingPlace = pollingPlace;
+    user.picturePath = picturePath;
+
+    await user.save();
+
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME});
+
+    res.status(200).json({token, user});
+
+  } catch (error) {
+    res.status(304).json({error: error.message})
+  }
+};
