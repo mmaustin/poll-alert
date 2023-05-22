915cfdf6f47c00e7faf87dbe99d66bf715a1efd6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-22 13:25:04 -0400

    set up observances routes

--- routes/observanceRoutes.js
+++ routes/observanceRoutes.js
@@ -1,1 +1,10 @@
-//get all, update, delete, alsoObserved (using the likes functionality)
+import express from 'express';
+import { getObservance, getObservances, updateObservance, alsoObserved, deleteObservance } from '../controllers/observances.js';
+
+const router = express.Router();
+
+router.route('/').get(getObservances);
+router.route('/:id').get(getObservance).patch(updateObservance).delete(deleteObservance);
+router.route('/:id/observed').patch(alsoObserved);
+
+export default router;
