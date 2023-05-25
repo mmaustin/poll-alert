b9278c3d4606ba5a0e9d7a9163e02520e611a6c1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-25 10:12:26 -0400

    wrapped theme provider and theme around app components in app js file

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,31 @@ import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ObservancePage from "scenes/observancePage";
 import StatePage from "scenes/statePage";
-
+import {createTheme} from "@mui/material/styles";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
 
 
 function App() {
 
+  const mode = useSelector(state => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage/>} />
-          <Route path='/home' element={<HomePage/>} />
-          <Route path='/state/:stateId' element={<StatePage/>} />
-          <Route path='/observance/:observanceId' element={<ObservancePage/>} />
-          {/* <Route path='/home' element={isAuth ? <HomePage/> : <Navigate to="/"/>} />
-          <Route path='/state/:stateId' element={isAuth ? <StatePage/> : <Navigate to="/"/>} />
-          <Route path='/observance/:observanceId' element={isAuth ? <ObservancePage/> : <Navigate to='/'/>} /> */}
-        </Routes>      
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route path='/' element={<LoginPage/>} />
+            <Route path='/home' element={<HomePage/>} />
+            <Route path='/state/:stateId' element={<StatePage/>} />
+            <Route path='/observance/:observanceId' element={<ObservancePage/>} />
+            {/* <Route path='/home' element={isAuth ? <HomePage/> : <Navigate to="/"/>} />
+            <Route path='/state/:stateId' element={isAuth ? <StatePage/> : <Navigate to="/"/>} />
+            <Route path='/observance/:observanceId' element={isAuth ? <ObservancePage/> : <Navigate to='/'/>} /> */}
+          </Routes>
+        </ThemeProvider>   
       </BrowserRouter>
     </div>
   );
