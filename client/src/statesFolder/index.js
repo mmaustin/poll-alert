7065179b6deef72833b5050466b21927e82d4b3f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-29 11:52:09 -0400

    filled out state object's information, savie the picture path

--- client/src/statesFolder/index.js
+++ client/src/statesFolder/index.js
@@ -3,452 +3,452 @@ const unitedStates = [
   {
     name: 'ALABAMA',
     _id: 'xlzqptycg1',
-    population: 0,
-    governor: '',
+    populationApprox: '5,100,000',
+    governor: 'Kay Ivey',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Tommy Tuberville', 'Katie Britt'],
+    totalReps: 7
   },
   {
     name: 'ALASKA',
     _id: 'xlzqptycg2',
-    population: 0,
-    governor: '',
+    populationApprox: '740,000',
+    governor: 'Mike Dunleavy',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Lisa Murkowski', 'Dan Sullivan'],
+    totalReps: 1
   },
   {
     name: 'ARIZONA',
     _id: 'xlzqptycg3',
-    population: 0,
-    governor: '',
+    populationApprox: '7,200,000',
+    governor: 'Katie Hobbs',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Kyrsten Sinema', 'Mark Kelly'],
+    totalReps: 9
   },
   {
     name: 'ARKANSAS',
     _id: 'xlzqptycg4',
-    population: 0,
-    governor: '',
+    populationApprox: '3,020,000',
+    governor: 'Sarah Huckabee Sanders',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Tom Cotton', 'John Boozman'],
+    totalReps: 4
   },
   {
     name: 'CALIFORNIA',
     _id: 'xlzqptycg5',
-    population: 0,
-    governor: '',
+    populationApprox: '39,200,000',
+    governor: 'Gavin Newsom',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Dianne Feinstein', 'Alex Padilla'],
+    totalReps: 52
   },
   {
     name: 'COLORADO',
     _id: 'xlzqptycg6',
-    population: 0,
-    governor: '',
+    populationApprox: '5,840,000',
+    governor: 'Jared Polis',
     flagPicturePath: '',
-    senators: ['', ''],
+    senators: ['Michael Bennet', 'John Hickenlooper'],
     totalReps: 0
   },
   {
     name: 'CONNECTICUT',
     _id: 'xlzqptycg7',
-    population: 0,
-    governor: '',
+    populationApprox: '3,610,000',
+    governor: 'Ned Lamont',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Richard Blumenthal', 'Chris Murphy'],
+    totalReps: 5
   },
   {
     name: 'DELAWARE',
     _id: 'xlzqptycg8',
-    population: 0,
-    governor: '',
+    populationApprox: '1,020,000',
+    governor: 'John Carney',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Tom Carper', 'Chris Coons'],
+    totalReps: 1
   },
   {
     name: 'FLORIDA',
     _id: 'xlzqptycg9',
-    population: 0,
-    governor: '',
+    populationApprox: '22,400,00',
+    governor: 'Ron DeSantis',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Marco Rubio', 'Rick Scott'],
+    totalReps: 28
   },
   {
     name: 'GEORGIA',
     _id: 'xlzqptycg10',
-    population: 0,
-    governor: '',
+    populationApprox: '10,800,000',
+    governor: 'Brian Kemp',
     flagPicturePath: '',
-    senators: ['', ''],
+    senators: ['Jon Ossoff', 'Raphael Warnock'],
     totalReps: 0
   },
   {
     name: 'HAWAII',
     _id: 'xlzqptycg11',
-    population: 0,
-    governor: '',
+    populationApprox: '1,500,000',
+    governor: 'Josh Green',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Brian Schatz', 'Mazie Hirono'],
+    totalReps: 2
   },
   {
     name: 'IDAHO',
     _id: 'xlzqptycg12',
-    population: 0,
-    governor: '',
+    populationApprox: '2,000,000',
+    governor: 'Brad Little',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Mike Crapo', 'Jim Risch'],
+    totalReps: 2
   },
   {
     name: 'ILLINOIS',
     _id: 'xlzqptycg13',
-    population: 0,
-    governor: '',
+    populationApprox: '12,820,000',
+    governor: 'J.B. Pritzker',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Dick Durbin', 'Tammy Duckworth'],
+    totalReps: 17
   },
   {
     name: 'INDIANA',
     _id: 'xlzqptycg14',
-    population: 0,
-    governor: '',
+    populationApprox: '6,800,000',
+    governor: 'Eric Holcomb',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Todd Young', 'Mike Braun'],
+    totalReps: 9
   },
   {
     name: 'IOWA',
     _id: 'xlzqptycg15',
-    population: 0,
-    governor: '',
+    populationApprox: '3,200,000',
+    governor: 'Kim Reynolds',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Chuck Grassley', 'Joni Ernst'],
+    totalReps: 4
   },
   {
     name: 'KANSAS',
     _id: 'xlzqptycg16',
-    population: 0,
-    governor: '',
+    populationApprox: '3,000,000',
+    governor: 'Laura Kelly',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Jerry Moran', 'Roger Marshall'],
+    totalReps: 4
   },
   {
     name: 'KENTUCKY',
     _id: 'xlzqptycg17',
-    population: 0,
-    governor: '',
+    populationApprox: '4,510,000',
+    governor: 'Andy Beshear',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Mitch McConnell', 'Rand Paul'],
+    totalReps: 6
   },
   {
     name: 'LOUISIANA',
     _id: 'xlzqptycg18',
-    population: 0,
-    governor: '',
+    populationApprox: '4,660,000',
+    governor: 'John Bel Edwards',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Bill Cassidy', 'John Kennedy'],
+    totalReps: 6
   },
   {
     name: 'MAINE',
     _id: 'xlzqptycg19',
-    population: 0,
-    governor: '',
+    populationApprox: '1,370,000',
+    governor: 'Janet Mills',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Susan Collins', 'Angus King'],
+    totalReps: 2
   },
   {
     name: 'MARYLAND',
     _id: 'xlzqptycg20',
-    population: 0,
-    governor: '',
+    populationApprox: '6,200,000',
+    governor: 'Wes Moore',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Ben Cardin', 'Chris Van Hollen'],
+    totalReps: 8
   },
   {
     name: 'MASSACHUSETTS',
     _id: 'xlzqptycg21',
-    population: 0,
-    governor: '',
+    populationApprox: '6,980,000',
+    governor: 'Maura Healey',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Elizabeth Warren', 'Ed Markey'],
+    totalReps: 9
   },
   {
     name: 'MICHIGAN',
     _id: 'xlzqptycg22',
-    population: 0,
-    governor: '',
+    populationApprox: '10,100,000',
+    governor: 'Gretchen Whitmer',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Debbie Stabenow', 'Gary Peters'],
+    totalReps: 13
   },
   {
     name: 'MINNESOTA',
     _id: 'xlzqptycg23',
-    population: 0,
-    governor: '',
+    populationApprox: '5,720,000',
+    governor: 'Tim Walz',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Amy Klobuchar', 'Tina Smith'],
+    totalReps: 8
   },
   {
     name: 'MISSISSIPPI',
     _id: 'xlzqptycg24',
-    population: 0,
-    governor: '',
+    populationApprox: '3,000,000',
+    governor: 'Tate Reeves',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Roger Wicker', 'Cindy Hyde-Smith'],
+    totalReps: 4
   },
   {
     name: 'MISSOURI',
     _id: 'xlzqptycg25',
-    population: 0,
-    governor: '',
+    populationApprox: '6,160,000',
+    governor: 'Mike Pearson',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Josh Hawley', 'Eric Schmitt'],
+    totalReps: 8
   },
   {
     name: 'MONTANA',
     _id: 'xlzqptycg26',
-    population: 0,
-    governor: '',
+    populationApprox: '120,000',
+    governor: 'Greg Gianforte',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Jon Tester', 'Steve Daines'],
+    totalReps: 2
   },
   {
     name: 'NEBRASKA',
     _id: 'xlzqptycg27',
-    population: 0,
-    governor: '',
+    populationApprox: '2,000,000',
+    governor: 'Jim Pillen',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Deb Fischer', 'Pete Ricketts'],
+    totalReps: 3
   },
   {
     name: 'NEVADA',
     _id: 'xlzqptycg28',
-    population: 0,
-    governor: '',
+    populationApprox: '3,100,000',
+    governor: 'Joe Lombardo',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Catherine Cortez Masto', 'Jacky Rosen'],
+    totalReps: 4
   },
   {
     name: 'NEW HAMPSHIRE',
     _id: 'xlzqptycg29',
-    population: 0,
-    governor: '',
+    populationApprox: '1,400,000',
+    governor: 'Chris Sununu',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Jeanne Shaneen', 'Maggie Hassan'],
+    totalReps: 2
   },
   {
     name: 'NEW JERSEY',
     _id: 'xlzqptycg30',
-    population: 0,
-    governor: '',
+    populationApprox: '9,300,000',
+    governor: 'Phil Murphy',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Bob Menendez', 'Cory Booker'],
+    totalReps: 12
   },
   {
     name: 'NEW MEXICO',
     _id: 'xlzqptycg31',
-    population: 0,
-    governor: '',
+    populationApprox: '2,120,000',
+    governor: 'Michelle Lujan Grisham',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Martin Heinrich', 'Ben Ray Lujan'],
+    totalReps: 3
   },
   {
     name: 'NEW YORK',
     _id: 'xlzqptycg32',
-    population: 0,
-    governor: '',
+    populationApprox: '20,200,000',
+    governor: 'Kathy Hochul',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Chuck Schumer', 'Kirsten Gillibrand'],
+    totalReps: 26
   },
   {
     name: 'NORTH CAROLINA',
     _id: 'xlzqptycg33',
-    population: 0,
-    governor: '',
+    populationApprox: '10,700,00-',
+    governor: 'Roy Cooper',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Thom Tillis', 'Ted Budd'],
+    totalReps: 14
   },
   {
     name: 'NORTH DAKOTA',
     _id: 'xlzqptycg34',
-    population: 0,
-    governor: '',
+    populationApprox: '780,000',
+    governor: 'Doug Burgum',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['John Hoeven', 'Kevin Cramer'],
+    totalReps: 1
   },
   {
     name: 'OHIO',
     _id: 'xlzqptycg35',
-    population: 0,
-    governor: '',
+    populationApprox: '11,800,000',
+    governor: 'Mike DeWine',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Sherrod Brown', 'J.D. Vance'],
+    totalReps: 15
   },
   {
     name: 'OKLAHOMA',
     _id: 'xlzqptycg36',
-    population: 0,
-    governor: '',
+    populationApprox: '4,020,000',
+    governor: 'Kevin Stitt',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['James Lankford', 'Markwayne Mullin'],
+    totalReps: 5
   },
   {
     name: 'OREGON',
     _id: 'xlzqptycg37',
-    population: 0,
-    governor: '',
+    populationApprox: '4,250,000',
+    governor: 'Tina Kotek',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Ron Wyden', 'Jeff Merkley'],
+    totalReps: 6
   },
   {
     name: 'PENNSYLVANIA',
     _id: 'xlzqptycg38',
-    population: 0,
-    governor: '',
+    populationApprox: '13,000,000',
+    governor: 'Josh Shapiro',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Bob Casey Jr.', 'John Fetterman'],
+    totalReps: 17
   },
   {
     name: 'RHODE ISLAND',
     _id: 'xlzqptycg39',
-    population: 0,
-    governor: '',
+    populationApprox: '1,100,000',
+    governor: 'Dan McKee',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Jack Reed', 'Sheldon Whitehouse'],
+    totalReps: 2
   },
   {
     name: 'SOUTH CAROLINA',
     _id: 'xlzqptycg40',
-    population: 0,
-    governor: '',
+    populationApprox: '5,284,000',
+    governor: 'Henry McMaster',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Lindsey Graham', 'Tim Scott'],
+    totalReps: 7
   },
   {
     name: 'SOUTH DAKOTA',
     _id: 'xlzqptycg41',
-    population: 0,
-    governor: '',
+    populationApprox: '910,000',
+    governor: 'Kristi Noem',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['John Thune', 'Mike Rounds'],
+    totalReps: 1
   },
   {
     name: 'TENNESSEE',
     _id: 'xlzqptycg42',
-    population: 0,
-    governor: '',
+    populationApprox: '6,915,000',
+    governor: 'Bill Lee',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Marsha Blackburn', 'Bill Hagerty'],
+    totalReps: 9
   },
   {
     name: 'TEXAS',
     _id: 'xlzqptycg43',
-    population: 0,
-    governor: '',
+    populationApprox: '29,300,000',
+    governor: 'Greg Abbott',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['John Cornyn', 'Ted Cruz'],
+    totalReps: 38
   },
   {
     name: 'UTAH',
     _id: 'xlzqptycg44',
-    population: 0,
-    governor: '',
+    populationApprox: '3,300,000',
+    governor: 'Spencer Cox',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Mike Lee', 'Mitt Romney'],
+    totalReps: 4
   },
   {
     name: 'VERMONT',
     _id: 'xlzqptycg45',
-    population: 0,
-    governor: '',
+    populationApprox: '650,000',
+    governor: 'Phil Scott',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Bernie Sander', 'Peter Welch'],
+    totalReps: 1
   },
   {
     name: 'VIRGINIA',
     _id: 'xlzqptycg46',
-    population: 0,
-    governor: '',
+    populationApprox: '8,700,000',
+    governor: 'Glenn Youngkin',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Mark Warner', 'Tim Kaine'],
+    totalReps: 11
   },
   {
     name: 'WASHINGTON',
     _id: 'xlzqptycg47',
-    population: 0,
-    governor: '',
+    populationApprox: '7,800,000',
+    governor: 'Jay Inslee',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Patty Murray', 'Maria Cantwell'],
+    totalReps: 10
   },
   {
     name: 'WEST VIRGINIA',
     _id: 'xlzqptycg48',
-    population: 0,
-    governor: '',
+    populationApprox: '1,794,000',
+    governor: 'Jim Justice',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Joe Manchin', 'Shelley Moore Capito'],
+    totalReps: 2
   },
   {
     name: 'WISCONSIN',
     _id: 'xlzqptycg49',
-    population: 0,
-    governor: '',
+    populationApprox: '5,900,000',
+    governor: 'Tony Evers',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['Ron Johnson', 'Tammy Baldwin'],
+    totalReps: 8
   },
   {
     name: 'WYOMING',
     _id: 'xlzqptycg50',
-    population: 0,
-    governor: '',
+    populationApprox: '577,000',
+    governor: 'Mark Gordon',
     flagPicturePath: '',
-    senators: ['', ''],
-    totalReps: 0
+    senators: ['John Barrasso', 'Cynthia Lummis'],
+    totalReps: 1
   },
 ];
 
