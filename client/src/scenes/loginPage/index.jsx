6042cec2a3d7feffe7c95d9bcae1781deaf0c2d2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-30 14:02:32 -0400

    tested template literal fetching of state picutre

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,13 @@
 import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
 
 
 const LoginPage = () => {
+
+  const states = useSelector(state => state.unitedStates);
+  const st = states.filter(s => s._id === 'xlzqptycg10');
+  const {flagPicturePath} = st[0];
+
   return (
     <>
       <div>Login</div>
@@ -11,7 +17,7 @@ const LoginPage = () => {
                 width='80px'
                 height='80px'
                 alt="user"
-                src='http://localhost:5001/assets/nevada.jpg'
+                src={`http://localhost:5001/assets/${flagPicturePath}`}
             />        
       </Box>
     </>
