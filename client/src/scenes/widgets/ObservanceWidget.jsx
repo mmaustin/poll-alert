ce1ff40d28830cddd5d103e5a6ab84a68adfd55a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-06-07 10:57:51 -0400

    completed observanc widget

--- client/src/scenes/widgets/ObservanceWidget.jsx
+++ client/src/scenes/widgets/ObservanceWidget.jsx
@@ -2,7 +2,6 @@ import { ChatBubbleOutlineOutlined, FavoriteBorderOutlined, FavoriteOutlined, Sh
 import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setObservance } from "state";
 
@@ -41,7 +40,43 @@ const ObservanceWidget = ({
   };  
 
   return (
-    <div>ObservanceWidget</div>
+    <WidgetWrapper m="2rem 0">
+      <div>
+        <Typography>{name}</Typography>
+        <Typography>{pollingPlace}</Typography>
+      </div>
+      <Typography>
+        {description}
+      </Typography>
+        {picturePath && (
+          <img
+            width='100%'
+            height='auto'
+            alt='post'
+            style={{borderRadius: '0.75rem', marginTop: '0.75ren'}}
+            src={`http://localhost:5001/assets/${picturePath}`}
+          />
+        )}
+        <FlexBetween mt='0.25rem'>
+          <FlexBetween gap='1rem'>
+
+            <FlexBetween gap='0.3rem'>
+              <IconButton onClick={patchObservance}>
+                {isLiked ? ( 
+                  <FavoriteOutlined sx={{color: primary}}/>
+                ) : (
+                  <FavoriteBorderOutlined />
+                )}
+              </IconButton>
+              <Typography>{observedCount}</Typography>
+            </FlexBetween>
+          </FlexBetween>
+
+          <IconButton>
+                <ShareOutlined/>
+          </IconButton>
+        </FlexBetween>
+    </WidgetWrapper>
   )
 }
 export default ObservanceWidget;
