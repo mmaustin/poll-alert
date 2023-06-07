489ea97477089a706393563d3943fb1eaaab6079
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-06-07 09:25:53 -0400

    created the non-jsx portion of observances widget

--- client/src/scenes/widgets/ObservancesWidget.jsx
+++ client/src/scenes/widgets/ObservancesWidget.jsx
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import state, { getObservances } from "state";
+import ObservanceWidget from "./ObservanceWidget";
 
 
+const ObservancesWidget = ({userId}) => {
 
-const ObservancesWidget = () => {
+  const dispatch = useDispatch();
+  const observances = useSelector(state => state.observances);
+  const token = useSelector(state => state.token);
+
+  const grabObservances = async () => {
+    const response = await fetch('http://localhost:5001/observances', {
+      method: 'GET',
+      headers: {Authorization: `Bearer ${token}`}
+    });
+    const data = await response.json();
+    dispatch(getObservances({observances: data}))
+  }
+
+  useEffect(() => {
+    grabObservances();
+  }, []) //eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div>ObservancesWidget</div>
   )
