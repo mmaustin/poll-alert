c627912655c408bc04cc4edea81d4114e4e9780d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-01 07:08:45 -0400

    removed google fonts address from css file to hopefully address csp issue

--- client/src/theme.js
+++ client/src/theme.js
@@ -75,30 +75,31 @@ export const colorTokens = {
             }),
       },
       typography: {
-        fontFamily: ["Rubik", "sans-serif"].join(","),
+        // fontFamily: ["Rubik", "sans-serif"].join(","),
+        fontFamily: ["Times New Roman", "sans-serif"].join(","),
         fontSize: 12,
         h1: {
-          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontFamily: ["Times New Roman", "sans-serif"].join(","),
           fontSize: 40,
         },
         h2: {
-          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontFamily: ["Times New Roman", "sans-serif"].join(","),
           fontSize: 32,
         },
         h3: {
-          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontFamily: ["Times New Roman", "sans-serif"].join(","),
           fontSize: 24,
         },
         h4: {
-          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontFamily: ["Times New Roman", "sans-serif"].join(","),
           fontSize: 20,
         },
         h5: {
-          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontFamily: ["Times New Roman", "sans-serif"].join(","),
           fontSize: 16,
         },
         h6: {
-          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontFamily: ["Times New Roman", "sans-serif"].join(","),
           fontSize: 14,
         },
       },
